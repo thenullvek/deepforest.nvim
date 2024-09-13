8d1adf3a9af05a16cafb9cda01ffce210b3ab9cb
Weihao Feng <the@nullvek.com>
2024-09-13 21:13:23 +0200

    WIP: adjusted ui highlights

--- lua/deepforest/create-theme.lua
+++ lua/deepforest/create-theme.lua
@@ -41,7 +41,7 @@ return function(color, color_special)
 
         fg_border = color.gray4,
         bg_colorcolumn = color.gray1,
-        bg_cursorline = color.gray2,
+        bg_cursorline = "#171717",
 
         bg_visual = color.gray8,
         fg_visual = color.black,
