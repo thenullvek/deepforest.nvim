bbc8a4d52718653482efbd26616a1821d1517536
Weihao Feng <the@nullvek.com>
2025-01-05 14:45:50 +0100

    WIP: fixed render-markdown.lua

--- lua/deepforest/plugin/render-markdown.lua
+++ lua/deepforest/plugin/render-markdown.lua
@@ -12,7 +12,7 @@ return function(theme)
             spec.ln("RenderMarkdownH4", "@markup.heading.4"),
             spec.ln("RenderMarkdownH5", "@markup.heading.5"),
             spec.ln("RenderMarkdownH6", "@markup.heading.6"),
-            spec.bg("RenderMarkdownCode", theme.render_markdown.code_bg),
+            spec.bg("RenderMarkdownCode", theme.plugin_render_markdown.code_bg),
         },
     }
 end
